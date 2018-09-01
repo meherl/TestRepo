fd0578dd66c7b1af3218480f23afb9b8a2099f8c
meherl <melippmann@mail.csuchico.edu>
2018-08-31 21:38:49 -0700

    moved source and set doxygen source code folder

---
+++ source/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using endl;
+
+/*! \brief Brief description.
+ *         Brief description continued.
+ *
+ *  Detailed description starts here.
+ */
+
+/*!
+ *hello world
+*/
+int main( ){
+    cout << "hello world" << endl;
+
+return 0;
+}
+
+
